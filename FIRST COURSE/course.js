fde009970d25a75f3f1233089053bc1d67ee926b
unknown <motilayo112@gmail.com>
2024-05-04 07:20:11 +0100

    update

---
+++ FIRST COURSE/course.js
@@ -0,0 +1,19 @@
+//console.log('hello world');
+//console.log('Olowokandi Oluwaseun Dorcas');
+
+//name = 'oluwatosin';
+//console.log(name);
+
+/*food = Number(prompt('Enter the amount of your food'));
+tipPercentage = Number(prompt('tip %?'))/100;
+tipAmount= food * tipPercentage;
+total = food + tipPercentage;
+console.log('tip Amount: ', tipAmount);
+console.log('total: ', total);
+let weather = prompt('what is the weather condition in your area');
+if (weather == 'rain') {
+  console.log('Grab your umbrella')
+}
+ else {
+  console.log('Grab your sunglasses')
+}*/
